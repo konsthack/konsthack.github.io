aa6cf53761204d2248de9dd79aef834c04d2caa8
konsthack <konsthack@gmail.com>
2016-08-04 23:19:14 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,6 +8,7 @@ $( document ).ready(function() {
             html_str = html_str + 
             "<div class='bs-callout bs-callout-danger'>" + 
             "<h3>Test: " + obj.tests[i].testName + 
+             "| <small> Last update: " + obj.tests[i].Date +"</small>"+ 
             "</h3><hr>"+
             "<h4>Assertion: " + obj.tests[i].Assert + "</h4><hr>"+
                 "<div class='alert alert-danger'>" +
