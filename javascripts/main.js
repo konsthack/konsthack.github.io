008dc8b72ff8a526c501edb57aab30c0022b9a39
plix <palle.torsson@gmail.com>
2016-08-04 21:17:54 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
         obj = jQuery.parseJSON( parsed );
         var html_str = ''
         $.each(obj.tests, function(i, item) {
-            html_str = html_str + "<div><h2>"+  obj.tests[i].testName + "</h2><hr><div class='bs-callout bs-callout-danger'> "+ obj.tests[i].Assert + " </div><hr><div class='alert alert-anger' role='alert'> <span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'>"+ obj.tests[i].Status +"</span></div></div>";
+            html_str = html_str + "<div><h2>"+  obj.tests[i].testName + "</h2><hr><div class='bs-callout bs-callout-danger'> "+ obj.tests[i].Assert + " </div> <div class='alert alert-anger' role='alert'> <span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'>"+ obj.tests[i].Status +"</span></div></div>";
         });
         $(".test-target").html(html_str); 
 });
