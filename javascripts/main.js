b688a0e45aa3b740e7da0ff51df9f3c3f998d240
plix <palle.torsson@gmail.com>
2016-08-04 17:30:00 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,10 @@
 console.log('This would be the main JS file.');
 $( document ).ready(function() {
     console.log( "ready!" );
+    var some_json = $(".jsoncontent").text(); 
+    console.log( some_json );
     
 });
+
+
+
