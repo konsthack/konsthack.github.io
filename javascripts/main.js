997763488b6e0058c45af3100a6ce979f455646b
plix <palle.torsson@gmail.com>
2016-08-04 19:16:48 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,10 +3,10 @@ $( document ).ready(function() {
     var some_json = $(".jsoncontent").text(); 
     console.log(some_json);
     console.log(typeof some_json)
-    some_json.replace(/=/g, ':'); 
-    some_json.replace(/>/g, ''); 
-    console.log( some_json );
-    var obj = jQuery.parseJSON( some_json );
+    var parsed = some_json.replace(/=>/g, ':'); 
+     
+    console.log(parsed );
+    var obj = jQuery.parseJSON( parsed );
     console.dir( obj.tests );
     console.dir( some_json );
     
