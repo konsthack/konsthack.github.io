8d319269806a2100883e040f46acd6c237ea3ce9
plix <palle.torsson@gmail.com>
2016-08-04 18:46:06 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,9 @@ $( document ).ready(function() {
     console.log( "ready!" );
     var some_json = $(".jsoncontent").text(); 
     console.log( some_json );
+    var obj = jQuery.parseJSON( some_json );
+    console.log( obj.tests );
+    console.log( some_json );
     
 });
 
