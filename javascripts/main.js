1b8841006acf1cb91145e9ce55cd88fbb7f4b4cb
konsthack <konsthack@gmail.com>
2016-08-04 21:39:16 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,7 +5,15 @@ $( document ).ready(function() {
         obj = jQuery.parseJSON( parsed );
         var html_str = ""
         $.each(obj.tests, function(i, item) {
-            html_str = html_str + "<h2>"+  obj.tests[i].testName + "</h2><hr><div class='bs-callout bs-callout-danger'><h4> "+ obj.tests[i].Assert + "</h4><div class='alert alert-anger' role='alert'> <span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'>"+ obj.tests[i].Status +"</span></div>";
+            html_str = html_str + 
+                            "<h2>" + obj.tests[i].testName + 
+                            "</h2><hr>"+
+                            "<div class='bs-callout bs-callout-danger'>" + 
+                            "<h4>" + obj.tests[i].Assert + 
+                            "</h4><div class='alert alert-anger'>" +
+                            "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'>"+ 
+                            obj.tests[i].Status +
+                            "</span></div></div>";
         });
         $(".test-target").html(html_str); 
 });
