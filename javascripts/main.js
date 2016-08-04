13207cc2b472212ae9e169fc632545bcbf739203
plix <palle.torsson@gmail.com>
2016-08-04 19:24:18 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,8 +3,13 @@ $( document ).ready(function() {
     var some_json = $(".jsoncontent").text(), 
         parsed = some_json.replace(/=>/g, ':'), 
         obj = jQuery.parseJSON( parsed );
-    
-    console.dir( obj.tests );
+        var html_str = ''
+        $.each(obj.tests, function(i, item) {
+            html_str = html_str + data[i].testName;
+        });
+        
+    $(".test-target").html(html_str); 
+ 
 });
 
 
