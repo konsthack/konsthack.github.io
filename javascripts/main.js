5f694e043b78d4c748f449dc912a7ccdc6cba844
konsthack <konsthack@gmail.com>
2016-08-04 21:55:12 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,12 +7,15 @@ $( document ).ready(function() {
         $.each(obj.tests, function(i, item) {
             html_str = html_str + 
                             "<h2>" + obj.tests[i].testName + 
+                            "<small> Last update" + obj.tests[i].Date +"</small>"+ 
                             "</h2><hr>"+
                             "<div class='bs-callout bs-callout-danger'>" + 
-                            "<h4>" + obj.tests[i].Assert + 
-                            "</h4><div class='alert alert-danger'>" +
-                            "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>" +
-                             obj.tests[i].Status + "</div></div>";
+                            "<h4>Assertion: " + obj.tests[i].Assert + "</h4>"+
+                            "<div class='alert alert-danger'>" +
+                                "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>" +
+                                "<span> "+ obj.tests[i].Status + " </span>" +
+                                "</div>"+
+                            "</div>";
         });
         $(".test-target").html(html_str); 
 });
