c64e38b05f02e553edbd2a04cf900831e5e35a9e
konsthack <konsthack@gmail.com>
2016-08-04 21:59:43 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,10 +10,15 @@ $( document ).ready(function() {
                             "</h2><hr>"+
                             "<div class='bs-callout bs-callout-danger'>" + 
                             "<h4>Assertion: " + obj.tests[i].Assert + "</h4>"+
-                            "<div class='alert alert-danger'>" +
-                                "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>" +
-                                "<span> "+ obj.tests[i].Status + " </span>" +
-                                "<small> Last update" + obj.tests[i].Date +"</small>"+ 
+                                "<div class='alert alert-danger'>" +
+                                    "<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>" +
+                                    "<span> "+ obj.tests[i].Status + " </span>" +
+                                    "<small> Last update" + obj.tests[i].Date +"</small>"+ 
+                                    "</div>"+
+                                "<div class='progress'>"+
+                                    "<div class='progress-bar progress-bar-success progress-bar-striped' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width: 40%'>"+
+                                        "<span class='sr-only'>40% Complete (success)</span>"+
+                                    "</div>"+
                                 "</div>"+
                             "</div>";
         });
