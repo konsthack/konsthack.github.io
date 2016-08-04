66b5a8e65cf426ebd1e673cc0606c469c55535e9
konsthack <konsthack@gmail.com>
2016-08-04 23:10:18 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -23,7 +23,7 @@ $( document ).ready(function() {
                 "</div>";
                 if (obj.tests[i].Images.length > 0) {
                     html_str = html_str + "<div>";
-                    $.each(obj.tests.Images, function(i, item) {
+                    $.each(obj.tests[i].Images, function(i, item) {
                         html_str = html_str + "<img src="+item+" >";
                     });
                     html_str = html_str + "</div>";
