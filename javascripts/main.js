bcd6ecd8fa7fa8e7ecda36bc3c4911988ec9fd62
plix <palle.torsson@gmail.com>
2016-08-04 18:56:45 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 
 $( document ).ready(function() {
     console.log( "ready!" );
-    ("=>").replaceAll(":");
+    $("=>").replaceAll(":");
    
     var some_json = $(".jsoncontent").text(); 
     
