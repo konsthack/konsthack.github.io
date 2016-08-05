c815715c0021ad0ad09c066f53b2e3b97b9687bf
konsthack <konsthack@gmail.com>
2016-08-05 11:46:59 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,8 +9,8 @@ $( document ).ready(function() {
         $.each(obj.tests, function(i, item) {
             html_str = html_str + 
             "<div class='bs-callout bs-callout-danger'>" + 
-            "<h4>"T_" + l + ": "+ obj.tests[i].testName + 
-             " | <small> Last update: " + obj.tests[i].Date +" | Test Id: "+ title + "_Test_" + l + " | </small>"+ 
+            "<h4>T_" + l + ": "+ obj.tests[i].testName + 
+             " | <small> Last update: " + obj.tests[i].Date + " | Test Id:  "+ title + "_Test_" + l + " | </small>" + 
             "</h4><hr>"+
             "<h5>Assertion: " + obj.tests[i].Assert + "</h5><hr>"+
                 "<div class='alert alert-danger'>" +
