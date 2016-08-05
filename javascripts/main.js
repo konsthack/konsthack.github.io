58e41fa64f827bc79c316bb783c4667fda5ec98d
konsthack <konsthack@gmail.com>
2016-08-05 12:04:02 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,17 +12,17 @@ $( document ).ready(function() {
             
                 if (obj.tests[i].Status == "Success") {
                     html_str = html_str +'info>'; 
-                    
+                    console.log("suss"); 
                 } else {
                     html_str = html_str +'danger>'; 
                 }    
             
-                html_str = html_str + 
-                "<h4>T_" + l + ": "+ obj.tests[i].testName + 
-                 " | <small> Last update: " + obj.tests[i].Date + " | Test Id:  "+ title + "_Test_" + l + " | </small>" + 
-                "</h4><hr>"+
-                "<h5>Assertion: " + obj.tests[i].Assert + "</h5><hr>"+
-                "<div class='alert alert-";
+            html_str = html_str + 
+            "<h4>T_" + l + ": "+ obj.tests[i].testName + 
+             " | <small> Last update: " + obj.tests[i].Date + " | Test Id:  "+ title + "_Test_" + l + " | </small>" + 
+            "</h4><hr>"+
+            "<h5>Assertion: " + obj.tests[i].Assert + "</h5><hr>"+
+            "<div class='alert alert-";
                 
                 if (obj.tests[i].Status == "Success") {
                     html_str = html_str +'info>'; }
