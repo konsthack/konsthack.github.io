4aaa60c21616e72ee8a62e71314cc928966b2d9d
konsthack <konsthack@gmail.com>
2016-08-07 11:54:04 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
         console.dir(list_active); 
         if (list_active != 0) {  
             $.each(list_active , function(i, item) {
-               console.log(list_active[i])
+               console.log(list_active[i].innerText)
             }); 
         }
         
