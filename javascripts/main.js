8edf071b972cf19b5147f1a445f2c1718b9b27ee
konsthack <konsthack@gmail.com>
2016-08-07 11:36:47 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,7 @@
 
 $( document ).ready(function() {
     // show list of resent tests
-    var list_active = $(".list").html() || 0;   
+    var list_active = $(".last").html() || 0;   
         
         if (list_active != 0) {  
             $.each(list_active, function(i, item) {
